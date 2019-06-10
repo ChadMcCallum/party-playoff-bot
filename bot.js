ba76efb8ef02b00e07f51db6db0cf68c233349f5
Chad McCallum <chad@mycomply.net>
2019-06-09 23:13:05 -0600

    A few tweaks to discord

--- bot.js
+++ bot.js
@@ -9,33 +9,12 @@ var ConsoleServer = function() {
     me.sendUser = (user, message) => {
         console.log(`Sending message ${message} to ${user}`);
     }
-    setTimeout(() => {
-        me.emit('message', 'Chad', 'pick', [1]);
-    }, 2000);
     return me;
 }
 util.inherits(ConsoleServer, emitter);
 
 game.addServer(new ConsoleServer());
 
-var discordServer = new DiscordServer();
-console.log(discordServer);
+var discordServer = new DiscordServer(process.env.token);
 
 game.addServer(discordServer);
-
-game.processMessage("Chad", "join");
-game.processMessage("Nathan", "join");
-game.processMessage("Chad", "pick", "1");
-game.processMessage("Nathan", "pick", "2")
-game.processMessage("Chad", "pick", "1");
-game.processMessage("Nathan", "pick", "2")
-game.processMessage("Chad", "pick", "1");
-game.processMessage("Nathan", "pick", "2")
-game.processMessage("Chad", "pick", "1");
-game.processMessage("Nathan", "pick", "2")
-game.processMessage("Chad", "pick", "1");
-game.processMessage("Nathan", "pick", "2")
-game.processMessage("Chad", "pick", "1");
-game.processMessage("Nathan", "pick", "2")
-game.processMessage("Chad", "pick", "1");
-game.processMessage("Nathan", "pick", "2")
--- discord.js
+++ discord.js
@@ -3,40 +3,45 @@ var util = require('util');
 var Discord = require('discord.io');
 // Initialize Discord Bot
 
-var DiscordServer = function() {
+function DiscordServer(token) {
     var me = this;
-
-    var userChannels = {};
-
-    var bot = new Discord.Client({
-        token: process.env.token,
+    this.bot = new Discord.Client({
+        token: token,
         autorun: true
     });
-    bot.on('ready', function (evt) {
+    this.messageQueue = [];
+    console.log(`Token: ${token}`);
+    this.bot.on('ready', function (evt) {
         console.log('Connected');
         console.log('Logged in as: ');
-        console.log(bot.username + ' - (' + bot.id + ')');
+        console.log(this.bot.username + ' - (' + this.bot.id + ')');
     });
-    bot.on('message', function (user, userID, channelID, message, evt) {
+    this.bot.on('message', function (user, userID, channelID, message, evt) {
         if (message.substring(0, 1) == '!') {
             var args = message.substring(1).split(' ');
             var cmd = args[0];           
             args = args.splice(1);
+            console.log(`Received message from ${userID} with command ${cmd} and args ${JSON.stringify(args)}`);
             me.emit('message', userID, cmd, args);
         }    
     });
-    me.sendUser = (user, message) => {
-        setTimeout(() => {
-            bot.sendMessage({
-                to: user,
-                message: message
+    setInterval(() => {
+        if(this.messageQueue.length > 0) {
+            var nextMessage = this.messageQueue.splice(0, 1);
+            this.bot.sendMessage({
+                to: nextMessage.user,
+                message: nextMessage.message
             });
-        }, 100);
-    };
-
-    return me;
+        }
+    }, 250);
 }
+util.inherits(DiscordServer, emitter);
+
+DiscordServer.prototype.sendUser = function sendUser(user, message) {
+    this.messageQueue.push({
+        "user": user,
+        "message": message
+    });    
+};
 
 module.exports = DiscordServer;
-
-util.inherits(DiscordServer, emitter);
