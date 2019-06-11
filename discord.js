ded70da49c4d781a1456ec70fb31c0184f52f169
Chad McCallum <chad@mycomply.net>
2019-06-10 21:29:27 -0600

    Swapped out what bot we're using

--- discord.js
+++ discord.js
@@ -1,6 +1,6 @@
 var emitter = require('events').EventEmitter;
 var util = require('util');
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 // Initialize Discord Bot
 
 const MessageDelay = 1000;
@@ -8,35 +8,30 @@ const MessageDelay = 1000;
 function DiscordServer(token) {
     var me = this;
     console.log("Token: " + token);
-    this.bot = new Discord.Client({
-        token: token,
-        autorun: false
-    });
+    this.bot = new Discord.Client();
     this.messageQueue = [];
-    this.bot.on('log', function(level, message) {
-        console.log(`${level}: ${message}`);
-    });
     this.bot.on('ready', function (evt) {
         console.log('Connected');
         console.log('Logged in as: ');
         console.log(me.bot.username + ' - (' + me.bot.id + ')');
     });
-    this.bot.on('message', function (user, userID, channelID, message, evt) {
-        if (message.substring(0, 1) == '!') {
-            var args = message.substring(1).split(' ');
+    this.bot.on('message', function (message) {
+        if (message.content.substring(0, 1) == '!') {
+            var args = message.content.substring(1).split(' ');
             var cmd = args[0];           
             args = args.splice(1);
-            console.log(`Received message from ${userID} with command ${cmd} and args ${JSON.stringify(args)}`);
-            me.emit('message', userID, cmd, args);
+            console.log(`Received message from ${message.author.id} with command ${cmd} and args ${JSON.stringify(args)}`);
+            me.emit('message', message.author.id, cmd, args);
         }    
     });
-    this.bot.connect();
+    this.bot.login(token);
     setInterval(() => {
         if(me.messageQueue.length > 0) {
             var nextMessage = me.messageQueue.splice(0, 1)[0];
-            me.bot.sendMessage({
-                to: nextMessage.user,
-                message: nextMessage.message
+            me.bot.fetchUser(nextMessage.user).then(user => {
+                user.createDM().then(channel => {
+                    channel.sendMessage(nextMessage.message);
+                });
             });
         }
     }, MessageDelay);
